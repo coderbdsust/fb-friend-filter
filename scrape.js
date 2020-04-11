a98d13cda42608ec53f9c3ce174f9440db988257
Biswajit Debnath <biswajit.sust@gmail.com>
2020-04-11 23:53:42 +0600

    Created caching mechanism for profile link retrieve (#30)
    * Created caching mechanism for profile link retrieve
    
    * Reformat & rename some props
    
    * Reorder features
    
    Co-authored-by: Biswajit Debnath <biswajit@kinetik.care>
    Co-authored-by: Sakibul Mowla <sakibulmowla@gmail.com>

--- scrape.js
+++ scrape.js
@@ -1,11 +1,27 @@
 require('console-stamp')(console, { pattern: 'dd/mm/yyyy HH:MM:ss.l' });
-
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 const config = require('./credential.json');
 
 (async () => {
+
     console.log("Starting Execution...");
+
+    let profileData = {};
+    const profileFilePath = `friend_${config.username}.json`;
+
+    try {
+        if (fs.existsSync(profileFilePath)) {
+            //file exists
+            console.log('Reading From Existing File.');
+            let rawdata = fs.readFileSync(profileFilePath);
+            profileData = JSON.parse(rawdata);
+        } else {
+            console.log('No Existing File Found!!!');
+        }
+    } catch (err) {
+        console.error(err);
+    }
 
     let browser;
     if (config.chromiumPath && config.chromiumPath !== '') {    
@@ -45,35 +61,55 @@ const config = require('./credential.json');
     // click on Log In
     await page.click('[value="Log In"]');
 
-    await sleep(10000);
-
+   // await sleep(10000);
     console.log('Login Done!!!');
+
+    await page.waitForSelector("[href='/me/']",{visiable: true});
 
     // goto friend list
     await page.goto('https://www.facebook.com/me/friends');
 
     await sleep(3000);
 
-    await autoScroll(page);
+    if (!profileData.allProfileFound) {
 
-    console.log('Starting to fetch allHrefs');
+        const startDate = new Date();
 
-    const allHrefs = await page.$$eval('a', anchors => anchors.map(anchor => anchor.href));
-    const ownerIdName = getOwnerIdName(page.url());
+        // Do your operations
+        await autoScroll(page);
 
-    console.log('allHrefs length:', allHrefs.length);
+        const endDate = new Date();
 
-    const friendProfileHrefs = filterValidFriendProfileHrefs(allHrefs, ownerIdName);
+        const seconds = (endDate.getTime() - startDate.getTime()) / 1000;
 
-    console.log('friendProfileHrefs length:', friendProfileHrefs.length);
-    console.log(JSON.stringify(friendProfileHrefs, null, 4));
+        console.log('Starting to fetch allHrefs');
 
-    // writing friend profile links to a file
-    const jsonProfileLinks = {
-        profiles: friendProfileHrefs
-    };
+        const allHrefs = await page.$$eval('a', anchors => anchors.map(anchor => anchor.href));
+        const ownerIdName = getOwnerIdName(page.url());
 
-    fs.writeFileSync('friend-profiles.json', JSON.stringify(jsonProfileLinks, null, 4));
+        console.log('allHrefs length:', allHrefs.length);
+
+        const friendProfileHrefs = filterValidFriendProfileHrefs(allHrefs, ownerIdName);
+
+        // writing friend profile links to a file
+        if (friendProfileHrefs && friendProfileHrefs.length > 0) {
+            profileData.allProfileFound = true;
+        } else {
+            profileData.allProfileFound = false;
+        }
+
+        profileData.profileLinkSearchTime = seconds;
+        profileData.profiles = friendProfileHrefs;
+      
+        console.log('Writig to file start');
+
+        fs.writeFileSync(profileFilePath, JSON.stringify(profileData, null, 4));
+
+        console.log('Writig to file end');
+
+    } else {
+        console.log('All friend profile link retrieved already');
+    }
 
     await browser.close();
 
@@ -101,30 +137,31 @@ const filterValidFriendProfileHrefs = (allHrefs, ownerIdName) => {
     const isOwnerRelatedPage = href => href.includes(ownerIdName);
     const isFeaturePage = (href) => {
         const features = [ 
-            'watch',
-            'marketplace',
-            'groups',
-            'gaming',
+            '?',
             'bookmarks',
-            'memories',
-            'videos',
-            'groups',
-            'posts',
-            'pages',
-            'events',
             'donate',
-            'photo',
+            'events',
+            'find-friends',
             'friends',
+            'gaming',
+            'groups',
             'groupslanding',
+            'marketplace',
+            'me/',
+            'memories',
             'messages',
             'mutual_friends',
-            'settings',
-            'notifications',
-            'profile',
-            'find-friends',
             'notes',
+            'notifications',
+            'pages',
             'permalink',
-            '?'
+            'photo',
+            'posts',
+            'profile',
+            'settings',
+            'stories',
+            'videos',
+            'watch'
         ];
 
         const matchesHref = feature => href.includes('/' + feature);
@@ -151,7 +188,7 @@ async function autoScroll(page) {
         await page.evaluate(async () => {
             await new Promise((resolve, reject) => {
                 let totalHeight = 0;
-                let distance = 400;
+                let distance = 350;
                 let timer = setInterval(() => {
                     let scrollHeight = document.body.scrollHeight;
                     window.scrollBy(0, distance);
